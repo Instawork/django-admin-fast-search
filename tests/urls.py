d51aa51cbbc983762a664879520e61105ef5d794
danaford <dana@toponium.com>
2024-08-19 14:52:56 -0700

    refactor(dj4): django.conf.urls.url() has been removed in 4.x

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.conf.urls import url, include
+from django.urls import re_path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    # url(r'^', include('django_admin_fast_search.urls', namespace='django_admin_fast_search')),
+    re_path(r"^admin/", admin.site.urls),
 ]
