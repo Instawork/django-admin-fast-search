e1c9f211fbede7ef031fa0f49748eb24eadd17b3
Dana Ford <dana@toponium.com>
2024-08-20 08:25:28 -0700

    refactor(dj4): upgrade to support django 4.2 (#14)
    * refactor(dj4): target tests against django 4.x
    
    * refactor(dj4): django.conf.urls.url() has been removed in 4.x
    
    * refactor(dj4): remove unused example test app
    
    * refactor(dj4): fix the models.W042 warning for defining auto field
    
    * refactor(dj4): run tox against django 4.x
    
    * refactor: bump version

--- django_admin_fast_search/urls.py
+++ django_admin_fast_search/urls.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
+from django.urls import re_path
 from django.views.generic import TemplateView
 
-from . import views
 
-
-app_name = 'django_admin_fast_search'
 urlpatterns = [
-    url(r'', TemplateView.as_view(template_name="base.html")),
-    ]
+    re_path(r"", TemplateView.as_view(template_name="base.html")),
+]
